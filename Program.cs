031c6d378c7bcee00e9386755334cc84e272fda2
Andressa Abrantes <andressasabrantes@gmail.com>
2023-11-03 09:32:34 -0300

    Projeto concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("895626262", "Pro Max", "4512368", 128);
+Nokia nokia = new Nokia("83964125", "A6 Plus", "45122599", 256);
+
+iphone.InstalarAplicativo("VSCO");
+nokia.InstalarAplicativo("Ligthroom");
